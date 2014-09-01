1c359d10f01fed71c6050cfcbdd3ca25ec2bb1e3
Cedric Thomas <cedric-thomas@hotmail.de>
2014-09-01 10:45:40 +0200

    new class for wordcount and the given functions for service

--- MapReduce/MapReduce/Program.cs
+++ MapReduce/MapReduce/Program.cs
@@ -8,42 +8,22 @@ namespace MapReduce
 {
     public class Program
     {
-        static MapReduce<KeyValuePair<string, string>> mr = new MapReduce<KeyValuePair<string, string>>();
-
-        static void lc_map(KeyValuePair<string, string> record)
-        {
-            string key = record.Key; //Buchtitel
-            string val = record.Value; //Alle Wörter im Buch / Wörter im Buchrücken o.ä.
-
-            foreach (string word in val.Split(' '))
-            {
-                mr.Emit_Intermediate(word, 1); // 1 bzw. Integer entspricht dem Typen in der generischen Liste in lc_reduce
-            }
-        }
-
-        static void lc_reduce(string key, List<object> values) // hier: object == integer
-        {
-            int total = 0; // Zu Beginn Wort 0 mal vorhanden
-
-            foreach (var val in values)
-            {
-                total++;                
-            }
-            
-            mr.Emit(string.Format("({0}, {1})", key, total)); //z.B.: (Auto, 1), (Fahrrad, 3), (hat, 2), ...
-        }
-
         static void Main(string[] args)
         {
-            List<KeyValuePair<string, string>> lc_in = new List<KeyValuePair<string, string>>();
-
-            lc_in.Add(new KeyValuePair<string, string>("Game of Thrones", "Everyone dies in the end end end"));
-            lc_in.Add(new KeyValuePair<string, string>("Metro2033", "Es ist das Jahr end 2033. Nach einem verheerenden Krieg liegen weite " +
-             "Teile der Welt in Schutt und Asche...."));
-            
-            mr.Execute(lc_in, lc_map, lc_reduce);
-
-            Console.ReadLine();
+            //Datei auslesen
+            //Multithreading beginnen
         }
     }
 }
+/*
+ * Multithreading einbauen
+ *  - 3 Bücher, je eins in einen Thread auf Service aufrufen, warten bis alle fertig sind zum weiteren abarbeiten (MAP)
+ *  - lokal zusammenfassen gleicher Worte (Werte addieren)
+ *  - alle Wörter mit mehr als 1 in neuem Thread auf Service aufrufen, warten bis freie Ressource und nächsten Aufruf starten bis alle abgearbeitet (REDUCE)
+ * Ausgabe auf Konsole
+ * WCF-Dienst einbinden
+ * 
+ * Präsentation
+ *  - Threading
+ *  - Verbindung zum Server von Client aus
+*/
---
+++ MapReduce/MapReduce/WordCount.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapReduce
+{
+    public static class WordCount
+    {
+        public static List<KeyValuePair<string, int>> Map(KeyValuePair<string, string> record)
+        {
+            List<KeyValuePair<string, int>> intermediate = new List<KeyValuePair<string, int>>();
+
+            string key = record.Key; //Buchtitel
+            string val = record.Value.Trim(); //Alle Wörter im Buch
+
+            foreach (string word in val.Split(' '))
+            {
+                intermediate.Add(new KeyValuePair<string, int>(word, 1));
+            }
+
+            return intermediate;
+        }
+        public static string Reduce(string key, List<int> values)
+        {
+            //Zu Beginn Wort 0 mal vorhanden
+            int total = 0;
+
+            foreach (var val in values)
+            {
+                total += val;
+            }
+
+            //z.B.: (Auto, 1), (Fahrrad, 3), (hat, 2), ...
+            return string.Format("({0}, {1})", key, total);
+        }
+    }
+}
